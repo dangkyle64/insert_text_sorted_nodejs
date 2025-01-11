41844d08ce58992f8b62e68bb7769b416c443ee2
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-11 06:46:38 -0800

    change filenames to follow nodejs filename style conventions, start readme
    author: dangkyle64
    date: 1/11/2025

---
+++ read-file-by-line.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline');
+
+function readFileByLine() {
+    const fileStream = fs.createReadStream('edit_this_file.rpy', 'utf8');
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        output: process.stdout,
+        terminal: false,
+    });
+    
+    rl.on('line', (line) => {
+        console.log('Line:', line);
+    });
+    
+    rl.on('error', (error) => {
+        console.error('Error reading file:', error);
+    });
+    
+    rl.on('close', () => {
+        console.log('File reading completed.');
+    });
+};
+
+readFileByLine();
--- read-whole-file-at-once.js
+++ read-whole-file-at-once.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-function readWholeFile() {
+// function reads the hardcoded test file in UTF-8 encoding 
+// returns the text data read from the test file on success, logging it into the console
+// returns an error message in the console on failure to read from file 
+function readWholeFileAtOnce() {
     fs.readFile('./edit_this_file.rpy', 'utf8', (error, data) => {
         if (error) {
             console.error('Error reading file:', error);
@@ -12,4 +15,4 @@ function readWholeFile() {
     });
 };
 
-readWholeFile();
+readWholeFileAtOnce();
